94d457f95e39e0a2d7f8b966c9d0946b325e4344
premananda1234 <92137599+premananda1234@users.noreply.github.com>
2023-03-17 00:19:14 +0530

    Prema Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Prema from './Prema'
 
 export default function App() {
   return (
     <div>
       <h1> Hy i am Susanta</h1>
+      <Prema/>
     </div>
   )
 }
---
+++ src/Prema.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Prema =()=>{
+    return(
+        
+        <div>
+            <h1> Hy i am Prema</h1>
+        </div>
+        
+    )
+}
+export default Prema;
